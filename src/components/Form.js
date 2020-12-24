8b85ca3d17ce5c5879be35fd94c5cb791ef7b44f
Paul_Beckwith <303dog@gmail.com>
2020-12-24 14:45:54 -0700

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,59 @@
-import React from 'react';
+import React from "react";
 
 class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
+    lastName: "Henry",
+    submittedData: []
+  };
+
+  handleFirstNameChange = (event) => {
+    this.setState({
+      firstName: event.target.value,
+    });
+  };
+
+  handleLastNameChange = (event) => {
+    this.setState({
+      lastName: event.target.value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const firstName = event.target.children[0].value
+    const lastName = event.target.children[1].value
+    this.sendFormDataSomewhere({ firstName, lastName })
   }
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
-
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
+  listOfSubmissions = () => {
+    return this.state.submittedData.map(data => {
+      return <div><span>{data.firstname}</span>
+      <span>{data.lastName}</span></div>
     })
   }
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input
+            type="text"
+            onChange={event => this.handleFirstNameChange(event)}
+            value={this.state.firstName}
+          />
+          <input
+            type="text"
+            onChange={event => this.handleLastNameChange(event)}
+            value={this.state.lastName}
+          />
+          <input type="submit"/>
+        </form>
+        {this.listOfSubmissions()}
+      </div>
     )
   }
 }
+
 
 export default Form;
